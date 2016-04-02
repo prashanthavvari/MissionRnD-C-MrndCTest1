58ae01a7d1fefec3b72ff9f5443976344c61c27a
prashanthavvari <prashanthavvari@missionrnd.com>
2016-04-02 21:23:19 +0530

    Submit Commit

--- src/CircularListsMerge.cpp
+++ src/CircularListsMerge.cpp
@@ -32,7 +32,79 @@ struct node{
 	int data;
 	struct node *next;
 };
+void insaf(node **h, int data1, int itr)
+{
+	node *temp1, *temp;
+	int i = 0;
+	temp = (node*)malloc(sizeof(node));
+	temp->data = data1;
+	temp-> = next;
+	if (*h == NULL)
+	{
+		*h = temp;
+	}
+	else
+	{
+		temp1 = *h;
+		while (temp1->next != *h&&i < itr - 1)
+		{
+			temp1 = temp1->next;
+			i++;
+		}
+		temp->next = temp1->next;
+		temp1->next = temp;
+	}
+}
+void mergmethd2(node **h1, node **h2)
+{
+	node *pointerh1, *pointerh2, *prev;
+	int i = 0, j = 0;
+	pointerh1 = *h1;
+	pointerh2 = *h2;
+	prev = *h1;
+	while (pointerh1->next != *h1&&pointerh2->next != *h2)
+	{
+		if (pointerh1->data < pointerh2->data)
+		{
+			pointerh1 = pointerh1->next;
+			i = i + 1;
+		}
+		else if (pointerh1->data > pointerh2->data)
+		{
+			j = pointerh2->data;
+			insaf(&prev, j, i);
+			pointerh2 = pointerh2->next;
+			i = i + 1;
+		}
+		else if (pointerh1->data == pointerh2->data)
+		{
+			pointerh1 = pointerh1->next;
+			pointerh2 = pointerh2->next;
+			i = i + 1;
+		}
+	}
+}
+int dis(struct node **h)
+{
+	node *pointer;
+	pointer = *h;
+	int i = 0;
+	while (pointer != *h)
+	{
+		
+		i++;
+	}
+	return i;
+}
 int merge_circularlists(struct node **head1, struct node **head2){
 	//Returns Length of merged Sorted circular SLL and also points *head1 to final SLL .
+	if (*head1==NULL&&*head2==NULL)
 	return -1;
+	else
+	{
+		int i = 0;
+		mergmethd2(head1, head2);
+		i=dis(head1);
+		return i;
+	}
 }
